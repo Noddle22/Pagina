6fe2fbd5968741561fcf98d797dda3b38e97ad31
Noddle22 <162944215+Noddle22@users.noreply.github.com>
2024-03-10 21:33:56 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function showContactInfo() {
+    const contactInfo = document.getElementById("contactInfo");
+    // Muestra u oculta la sección de información de contacto
+    contactInfo.style.display = contactInfo.style.display === "none" ? "block" : "none";
+}
